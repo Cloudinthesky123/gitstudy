bb7212c1c8e68ee136fa54389ed431d27e7f775e
yun <yun@123.com>
2019-07-08 23:02:30 +0800

    uptate

--- src/com/atguigu/redis/test/TestPing.java
+++ src/com/atguigu/redis/test/TestPing.java
@@ -5,6 +5,7 @@ import redis.clients.jedis.Jedis;
 public class TestPing {
 	public static void main(String[] args) 
 	{
+		System.out.println("hello");
 		Jedis jedis = new Jedis("127.0.0.1",6379);
 		System.out.println(jedis.ping());
 	}
